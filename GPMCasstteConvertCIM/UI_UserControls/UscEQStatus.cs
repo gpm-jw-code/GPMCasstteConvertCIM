8f2ded40988dcf75f958a33c5210fec721fe34f5
jinwei <jinweihuang@gpmcorp.com.tw>
2023-05-09 07:55:33 +0800

    datagridview change backcolor when state change

--- GPMCasstteConvertCIM/UI_UserControls/UscEQStatus.cs
+++ GPMCasstteConvertCIM/UI_UserControls/UscEQStatus.cs
@@ -22,8 +22,37 @@ namespace GPMCasstteConvertCIM.UI_UserControls
         public UscEQStatus()
         {
             InitializeComponent();
+            dataGridView1.CellFormatting += DataGridView1_CellFormatting;
 
+        }
 
+        private void DataGridView1_CellFormatting(object? sender, DataGridViewCellFormattingEventArgs e)
+        {
+            if (e.ColumnIndex >= 2 && e.RowIndex != -1)
+            {
+                clsConverterPort data = (clsConverterPort)dataGridView1.Rows[e.RowIndex].DataBoundItem;
+
+                bool state_to_change = false;
+
+                bool load_request = data.LoadRequest;
+                bool unload_request = data.UnloadRequest;
+
+                if (e.ColumnIndex == 2)
+                    state_to_change = data.LoadRequest;
+                if (e.ColumnIndex == 3)
+                    state_to_change = data.UnloadRequest;
+                if (e.ColumnIndex == 4)
+                    state_to_change = data.PortExist;
+                if (e.ColumnIndex == 5)
+                    state_to_change = data.LD_UP_POS;
+                if (e.ColumnIndex == 6)
+                    state_to_change = data.LD_DOWN_POS;
+                if (e.ColumnIndex == 7)
+                    state_to_change = data.PortStatusDown;
+
+                dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Style.BackColor = state_to_change ? Color.Lime : Color.White;
+
+            }
         }
 
         internal void BindData(List<clsConverterPort> allEqPortList)
