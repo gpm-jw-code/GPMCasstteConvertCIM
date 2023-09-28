140119f461f05b6daf264549f0626ed0a7311fb2
jinweihuang <jinweihuang2033@outlook.com>
2023-09-28 08:52:56 +0800

    alarm queue size limit

--- GPMCasstteConvertCIM/Alarm/AlarmManager.cs
+++ GPMCasstteConvertCIM/Alarm/AlarmManager.cs
@@ -20,6 +20,9 @@ namespace GPMCasstteConvertCIM.Alarm
         {
             if (AlarmCodes.TryGetValue(alarm_code, out clsAlarmDto alarmDeffined))
             {
+                if (AlarmsList.Count >= 50)
+                    AlarmsList.TryDequeue(out var oldest);
+
                 var newAalrm = new clsAlarmDto()
                 {
                     Classify = alarmDeffined.Classify,
@@ -45,6 +48,8 @@ namespace GPMCasstteConvertCIM.Alarm
             //AlarmList.Add(alarm);
             if (AlarmCodes.TryGetValue(alarm_code, out clsAlarmDto alarmDeffined))
             {
+                if (AlarmsList.Count >= 50)
+                    AlarmsList.TryDequeue(out var oldest);
                 var newAalrm = new clsAlarmDto()
                 {
                     Classify = alarmDeffined.Classify,
