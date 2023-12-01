06ce8bd57a7a81a1f39684712da78bd27d49df30
YouLinTsai <karta083729@gmail.com>
2023-12-01 17:06:15 +0800

    新增modbus dio class，對應se mp3播放需求功能

---
+++ GPMCasstteConvertCIM/AlarmDevice/clsAgvsAlarmDevice.cs
@@ -0,0 +1,44 @@
+﻿using GPMCasstteConvertCIM.GPM_Modbus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modbus.Device;
+using Modbus.Data;
+using System.Net.Sockets;
+using System.Net;
+using GPMCasstteConvertCIM.Utilities.SysConfigs;
+
+namespace GPMCasstteConvertCIM.AlarmDevice
+{
+    internal class clsAgvsAlarmDevice
+    {
+        clsModbusDeviceConfigs clsModbusDeviceConfigscls = new clsModbusDeviceConfigs();
+        ModbusIpMaster modbusMaster;
+        bool[] outputs = new bool[8];
+        string IPaddress => clsModbusDeviceConfigscls.IP_Address;
+        int Port => clsModbusDeviceConfigscls.port;
+
+        internal void Initialize()
+        {
+            AdamConnect();
+        }
+
+        private void AdamConnect()
+        {
+            var tcp_client = new TcpClient(IPaddress, Port);
+            modbusMaster =ModbusIpMaster.CreateIp(tcp_client);
+        }
+
+        internal void PlayMusic()
+        { 
+            //0 1 1 0 0 0 0 0  
+            modbusMaster.WriteMultipleCoilsAsync(1, new bool[] { true,true,false});//寫入特定io點位
+        }
+        internal void GetstopMusic() 
+        {
+            modbusMaster.ReadInputsAsync(0,8);
+        }
+    }
+}
---
+++ GPMCasstteConvertCIM/Utilities/SysConfigs/clsModbusDeviceConfigs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json.Converters;
+
+
+namespace GPMCasstteConvertCIM.Utilities.SysConfigs
+{
+    public class clsModbusDeviceConfigs
+    {
+        public string IP_Address { get; set; } = "127.0.0.1";
+        public int port { get; set; } = 6000;
+    }
+}
+
+
