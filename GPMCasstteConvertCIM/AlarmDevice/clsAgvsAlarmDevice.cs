badd4f8de2e0c54d97c7e83de988447ee49a9c4b
YouLinTsai <karta083729@gmail.com>
2023-12-06 17:37:15 +0800

    DIO對應因檔IO輸出點位功能

--- GPMCasstteConvertCIM/AlarmDevice/clsAgvsAlarmDevice.cs
+++ GPMCasstteConvertCIM/AlarmDevice/clsAgvsAlarmDevice.cs
@@ -9,6 +9,8 @@ using Modbus.Data;
 using System.Net.Sockets;
 using System.Net;
 using GPMCasstteConvertCIM.Utilities.SysConfigs;
+using GPMCasstteConvertCIM;
+
 
 namespace GPMCasstteConvertCIM.AlarmDevice
 {
@@ -16,10 +18,13 @@ namespace GPMCasstteConvertCIM.AlarmDevice
     {
         clsModbusDeviceConfigs clsModbusDeviceConfigscls = new clsModbusDeviceConfigs();
         ModbusIpMaster modbusMaster;
-        bool[] outputs = new bool[8];
         string IPaddress => clsModbusDeviceConfigscls.IP_Address;
         int Port => clsModbusDeviceConfigscls.port;
         bool IsDioCon;
+        bool[] outputs = new bool[7];
+        bool[] intputs = new bool[8];
+
+
 
         internal void Initialize()
         {
@@ -32,18 +37,46 @@ namespace GPMCasstteConvertCIM.AlarmDevice
             modbusMaster =ModbusIpMaster.CreateIp(tcp_client);
         }
 
-        internal void PlayMusic()
-        { 
-            //0 1 1 0 0 0 0 0  
-            modbusMaster.WriteMultipleCoilsAsync(1, new bool[] { true,true,false});//寫入特定io點位
-        }
-        internal void GetstopMusic() 
+        internal async void MusicStop()
         {
-            modbusMaster.ReadInputsAsync(0,8);
+            AdamConnect();
+            await modbusMaster.WriteMultipleCoilsAsync(16, new bool[] { false, false, false, false, false, false, false });
+        }
+        internal async void PlayAGV_AlarmMusic()
+        {
+            AdamConnect();
+            //0 1 1 0 0 0 0 0  
+            await modbusMaster.WriteMultipleCoilsAsync(18, new bool[] { true });//寫入特定io點位
+            //modbusMaster.WriteSingleCoil(17, true);//寫入特定io點位
+            //var di_inputs = modbusMaster.ReadInputs(0, 8);
+        }
+        internal async void offline()
+        {
+            AdamConnect();
+            await modbusMaster.WriteMultipleCoilsAsync(16, new bool[] { true });
+        }
+        internal async void Return_Online()
+        {
+            AdamConnect();
+            await Task.Delay(10);
+            await modbusMaster.WriteMultipleCoilsAsync(17, new bool[] { true });
+            await Task.Delay(1000);
+            await modbusMaster.WriteMultipleCoilsAsync(17, new bool[] { false });
+        }
+        internal void GetstopMusic()
+        {
+            AdamConnect();
+            modbusMaster.ReadCoilsAsync(0, 8);
+            bool[] inputStatus = modbusMaster.ReadInputs(0, 8);
+            for (int i = 0; i < intputs.Length; i++)
+            {
+                Console.WriteLine($"DI {0 + i}: {inputStatus[i]}");
+            }
         }
         internal async void Conn()
         {
-            do
+            
+            while (true) 
             {
                 using (TcpClient client = new TcpClient())
                 {
@@ -64,12 +97,12 @@ namespace GPMCasstteConvertCIM.AlarmDevice
                     {
                         Console.WriteLine(ex.Message);
                         IsDioCon = false;
-                        break;
                     }
-                    return;
+                    Thread.Sleep(1000);
+                    Console.WriteLine(IsDioCon);
                 }
             }
-            while(true);
+            
         }
     }
 }
