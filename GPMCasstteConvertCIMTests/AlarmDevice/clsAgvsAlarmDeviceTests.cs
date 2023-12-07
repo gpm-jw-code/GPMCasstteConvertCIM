72a8d2e53a039f415e27fa8706ef01804c15532d
jinweihuang <jinweihuang2033@outlook.com>
2023-12-07 09:31:41 +0800

    新增警報器模組單元測試

---
+++ GPMCasstteConvertCIMTests/AlarmDevice/clsAgvsAlarmDeviceTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GPMCasstteConvertCIM.AlarmDevice;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPMCasstteConvertCIM.AlarmDevice.Tests
+{
+    [TestClass()]
+    public class clsAgvsAlarmDeviceTests
+    {
+        [TestMethod()]
+        public void offlineTest()
+        {
+            clsAgvsAlarmDevice agv_buzzer = new clsAgvsAlarmDevice();
+            agv_buzzer.offline();
+            Thread.Sleep(1000);
+            agv_buzzer.Return_Online();
+            Thread.Sleep(1000);
+            agv_buzzer.GetstopMusic();
+            Thread.Sleep(1000);
+        }
+    }
+}
