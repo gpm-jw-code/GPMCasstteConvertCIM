bcb370e94ce1f80a1c85cd1e71868b825efb25e5
YouLinTsai <karta083729@gmail.com>
2023-12-08 17:46:21 +0800

    新增modbus參數檔

--- GPMCasstteConvertCIM/Utilities/SysConfigs/clsModbusDeviceConfigs.cs
+++ GPMCasstteConvertCIM/Utilities/SysConfigs/clsModbusDeviceConfigs.cs
@@ -14,6 +14,12 @@ namespace GPMCasstteConvertCIM.Utilities.SysConfigs
     {
         public string IP_Address { get; set; } = "192.168.1.100";
         public int port { get; set; } = 502;
+        public ushort DI_SrartAddress { get; set; } = 0;
+        public ushort DI_EndAddress { get; set; } = 7;
+
+        public ushort DO_SrartAddress { get; set; } =16 ;
+        public ushort DO_EndAddress { get; set; } = 22;
+        public bool Enable = false;
     }
 }
 
