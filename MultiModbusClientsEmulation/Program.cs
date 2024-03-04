892bf80fa74fcbead038d0a7cabd8a57dae0e181
jinweihunag <jinweihuang@gpmcorp.com.tw>
2024-03-04 18:38:24 +0800

    Optimized

---
+++ MultiModbusClientsEmulation/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MultiModbusClientsEmulation;
+using System.Runtime.CompilerServices;
+
+Console.Title = "Modbus Master 模擬器-[Connect to CIM]";
+
+int portStart = 1501;
+for (int i = 0; i < 24; i++)
+{
+    var _port = portStart + i;
+    ModbusClient client = new ModbusClient(_port);
+    _ = client.RunAsync();
+}
+
+
+while (true)
+{
+    Thread.Sleep(1);
+}
