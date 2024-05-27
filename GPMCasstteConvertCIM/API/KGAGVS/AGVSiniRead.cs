de6e9bcd6368ba9a0a55264f9371db4f093f34de
YouLinTsai <karta083729@gmail.com>
2024-05-27 16:09:20 +0800

    修正ini檔案若不存在建立新的不會跳出占用，若已存在則直接覆蓋檔案

--- GPMCasstteConvertCIM/API/KGAGVS/AGVSiniRead.cs
+++ GPMCasstteConvertCIM/API/KGAGVS/AGVSiniRead.cs
@@ -16,12 +16,12 @@ namespace GPMCasstteConvertCIM.API.KGAGVS
             string EQ_NameInini = EQ_Name.ToUpper() + "_"+Slot.ToString();
             string iniFilePath = @"c:\CST\ini\Status.ini";
             string NewiniFilePath = @"d:\cimfile\Status.ini";
-            if(!File.Exists(NewiniFilePath)) { File.Create(NewiniFilePath); }
+            if(!File.Exists(NewiniFilePath)) 
+            { using (FileStream fs = File.Create(NewiniFilePath)) ; }
             File.Copy(iniFilePath, NewiniFilePath, true);
             var parser = new FileIniDataParser();
             IniData data = parser.ReadFile(NewiniFilePath);
             lastCarrierID = data[EQ_NameInini]["LotID"]; //data[RACK3_1]["LotID"];
-            File.Delete(NewiniFilePath);
         }
         //public async static void checkinilastwrite()
         //{
